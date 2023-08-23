d4fb5e244b938ddc478c273294d19c86dd031933
vivekrajcvbnmkl <vivekraj7571@gmail.com>
2023-08-23 17:54:50 +0530

    simple react with data is fetching is on react app

--- frontend/hello-world/src/App.js
+++ frontend/hello-world/src/App.js
@@ -1,16 +1,51 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
+// import Greet from './components/Greet';
 import axios from 'axios'
+import React from 'react';
 
-import Welcome from './components/Welcome';
-function App() {
-  return (
-    <div className="App">
-      <Greet/>
-      <Welcome/>
-    </div>
-  );
+// import Welcome from './components/Welcome';
+
+
+class App extends React.Component{
+    state = { details: [],}
+
+    componentDidMount(){
+      let data;
+      axios.get('http://127.0.0.1:8000/')
+      .then(res => {
+        data = res.data;
+        this.setState({
+          details : data
+        });
+      })
+      .catch(err => {"error occuured"})
+
+    }
+    render(){
+      return (
+        <div>
+        <header>Data from django backend</header>
+        <hr></hr>
+        {this.state.details.map((output,id)=>(
+          <div key  = {id}>
+            <div>
+            <h3>{output.code}</h3>
+            <h3>{output.name}</h3>
+            <h3>{output.open}</h3>
+            <h3>{output.high}</h3>
+            <h3>{output.low}</h3>
+            <h3>{output.close}</h3>
+            <hr></hr>
+            </div>
+          </div>
+        ))}
+        </div>
+
+      )
+    }
+  
 }
+
 
 export default App;
